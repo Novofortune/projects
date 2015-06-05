8bdb050e8362d516e16e26b5aa4cb572deb8a0c3
Jerry12321 <hzrtnt018@126.com>
2015-06-06 00:42:25 +1000

    FileExplorer
    FileExplorer to help get a tree view of any given directory

---
+++ scriptmaster_c#/winformVersion/FileManager/FileManager/FileManager/FileOperation.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace FileManager
+{
+    public class FileTreeNode:TreeNode
+    {
+        public FileNode fileNode;
+        public static FileTreeNode LoadTreeView(FileNode fn)
+        {
+            FileTreeNode treeNode = new FileTreeNode();
+            treeNode.Text = fn.name;
+            treeNode.fileNode = fn;
+            if (fn.children != null)
+            {
+                for(int  i = 0;i<fn.children.Count;i++){
+                    FileTreeNode childTreeNode = LoadTreeView(fn.children[i]);
+                    treeNode.Nodes.Add(childTreeNode);
+                    
+                }
+                
+            }
+            return treeNode;
+        }
+    }
+    public class FileOperation
+    {
+        public static List<FileNode> GetFileTree(string path){
+            List<FileNode> nodes = new List<FileNode>();
+            FileNode fn = GetFileTreeLoop(path);
+            GetFileTreeLoop(fn,ref nodes);
+            return nodes;
+        }
+        private static void GetFileTreeLoop(FileNode node, ref List<FileNode> nodes) {
+            nodes.Add(node);
+            if (node.children!=null)
+            {
+                for (int i = 0; i < node.children.Count; i++)
+                {
+                    GetFileTreeLoop(node.children[i],ref nodes);
+                }
+            }
+        }
+        private static FileNode GetFileTreeLoop(string path) {
+            FileNode node = new FileNode();
+            DirectoryInfo dir = new DirectoryInfo(path);
+            node.xpath = dir.FullName;
+            node.isdir = dir.Attributes.HasFlag(FileAttributes.Directory); // Check if the path is a directory
+            node.name = dir.Name;
+            node.extension = dir.Extension;
+
+            if (node.isdir) 
+            {
+                node.children = new List<FileNode>();
+                FileSystemInfo[] fsi = dir.GetFileSystemInfos();
+                for (int i = 0; i < fsi.Length; i++)
+                {
+                    FileNode childNode = GetFileTreeLoop(fsi[i].FullName);
+                    node.children.Add(childNode);
+                    childNode.parent = node;
+                }
+            }
+                
+
+            return node;
+        }
+        
+    }
+    public class FileNode{
+        public FileNode parent;
+        public List<FileNode> children;
+        public string xpath;
+        public bool isdir;
+        public string name;
+        public string extension;
+    }
+}
---
+++ scriptmaster_c#/winformVersion/FileManager/FileManager/FileManager/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace FileManager
+{
+    class Program
+    {
+        public static bool running; 
+        static void Main(string[] args)
+        {
+
+            //___________Testing__________
+          //  string dir = Directory.GetCurrentDirectory();
+           //List<FileNode> fns =  FileOperation.GetFileTree(dir);
+          // for (int i = 0; i < fns.Count; i++)
+          // {
+          //     if (fns[i].parent != null)
+          //     {
+          //         Console.WriteLine(fns[i].parent.name);
+          //     }
+         //  }
+            //___________Testing__________
+
+
+
+            string curdir = Directory.GetCurrentDirectory();
+            List<FileNode> fns = FileOperation.GetFileTree(curdir);
+
+            Form1 form = new Form1(); 
+            form.FormClosed += form_FormClosed;
+            form.textBox1.Text = curdir;
+            FileTreeNode ftn = FileTreeNode.LoadTreeView(fns[0]);
+            form.treeView1.Nodes.Add(ftn);
+            form.Show();
+            running = true;
+            while (running)
+            {
+                Application.DoEvents();
+                Thread.Sleep(20);
+            }
+        }
+
+        static void form_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            running = false;
+        }
+    }
+}
